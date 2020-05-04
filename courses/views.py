c3d566bb008295447e3771abfb30811f373670ee
0ndreu <andrey.artyushov@yandex.ru>
2020-05-05 00:29:33 +0300

    CoursesViewSet with courses that user created

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import *
 from .models import *
 
@@ -7,4 +9,10 @@ from .models import *
 class CoursesViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-# Create your views here.
+
+    @action(methods=['get'], detail=True)  # add permission classes
+    def my_courses(self, request, pk=None):
+        queryset = Course.objects.filter(creator=self.request.user)
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
