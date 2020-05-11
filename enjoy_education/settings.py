1de6276c47d49c55fb2c42f7af643c31753ed91b
0ndreu <andrey.artyushov@yandex.ru>
2020-05-11 19:05:24 +0300

    fix

--- enjoy_education/settings.py
+++ enjoy_education/settings.py
@@ -43,6 +43,10 @@ DATABASES = {
 }
 
 
+# djang-heroku setting
+import django_heroku
+django_heroku.settings(locals())
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
